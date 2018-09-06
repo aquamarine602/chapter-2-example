e17ac2b838b41684f53bd1e0ddfa57a16123ccf1
chloe.spilker <chloe.spilker@CLC-LAB209-04.TCSD.tooelesd.org>
2018-09-06 14:00:28 -0600

    chapter 2 examples

---
+++ src/cspik3/chapter_two/examples/Chapter_2_examples.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package cspik3.chapter_two.examples;
+
+/**
+ * @author chloe.spilker
+ * 9/6/18
+ * examples from chapter 2
+ */
+
+public class Chapter_2_examples {
+	
+	/**
+	 * NOTES:
+	 * everything between curly brackets is part of class: for every opening, there is a closing
+	 * public static void is where program starts
+	 * methods inside classes
+	 * no functions in Java: methods
+	 * every statement ends in semicolon
+	 * (): where the argument is placed - in methods
+	 * String[]: an array, lets us use string methods
+	 */
+
+	public static void main(String[] args) {
+		// displays text
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to the jungle.");
+		System.out.println(4-2);
+		System.out.println("");
+		
+		// integer variables?
+		int jungle = 2;
+		System.out.println("Jungle is " + jungle + ".");
+		System.out.println(jungle - 1);
+	}
+
+}
+
+
+
+/**
+ * TYPES OF ERRORS
+ * 1. syntax
+ * 		something wrong with code syntax
+ * 		example: no curly brackets
+ * 
+ * 2. logical
+ * 		something wrong with code content
+ * 		example: 1/0
+ * 
+ * 3. runtime
+ * 		while code is running
+ * 		example: bugs
+ */
+
+
+
